d5b3d2131126cbb964848efd3dca1439c52989e9
lorenzo dicara <dicara97@live.it>
2023-07-03 16:25:39 +0200

    push

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
+import { Observable, Observer } from 'rxjs';
+
+const getBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
+  new Promise((resolve, reject) => {
+    console.log(file)
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +20,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  data = [
+    {
+      title: 'Title 1',
+      img: {url: undefined}
+    },
+    {
+      title: 'Title 2',
+      img: {url: undefined}
+    },
+    {
+      title: 'Title 3',
+      img: {url: undefined}
+    },
+    {
+      title: 'Title 4',
+      img: {url: undefined}
+    },
+    {
+      title: 'Title 5',
+      img: {url: undefined}
+    },
+    {
+      title: 'Title 6',
+      img: {url: undefined}
+    }
+  ];
+  fileList: NzUploadFile[] = [
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: "",
+    }
+  ];
+  previewImage: string | undefined = '';
+  previewVisible = false;
+  constructor(private msg: NzMessageService) { }
 
   ngOnInit() {
+    let data = JSON.parse(localStorage.getItem('data') || '{}')
+    if(data)
+    this.data = data
+  }
+
+
+  handlePreview = async (file: NzUploadFile): Promise<void> => {
+    console.log(file)
+    if (!file.url && !file['preview']) {
+      file['preview'] = await getBase64(file.originFileObj!);
+    }
+    this.previewImage = file.url || file['preview'];
+    this.previewVisible = true;
+  };
+
+  handleChange(event: any, index: number){
+    this.data[index].img = {
+      url: event.file.thumbUrl
+    }
+    localStorage.setItem('data', JSON.stringify(this.data))
   }
 
 }
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -4,10 +4,17 @@ import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
+import { CommonModule } from '@angular/common';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @NgModule({
-  imports: [WelcomeRoutingModule, NzCardModule],
+  imports: [WelcomeRoutingModule, NzCardModule,NzListModule,NzGridModule, CommonModule, NzUploadModule, NzMessageModule, NzIconModule, NzModalModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
